348bd8acda2cb707166f9b3611d383bd7c5d8f07
Nicholas Haisler <nicholas.haisler@drake.edu>
2025-12-02 14:50:17 -0600

    few changes

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,14 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const cors = require('cors');
-app.use(cors());
 
 // Middleware
-app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  })
+);
 
 // MongoDB Connection
 mongoose
